039fd84a04f4332bb3de829fbbfd3d65ec6c3cd5
mayapatech-a11y <mayapatech@gmail.com>
2025-11-16 21:18:09 +0800

    Update ultimate_script_menu.lua

--- ultimate_script_menu.lua
+++ ultimate_script_menu.lua
@@ -1,1 +1,2 @@
+--[[If you want to buy the whole script, dm me on discord and we will proccess it]]
 loadstring(game:HttpGet("https://gist.githubusercontent.com/mayapatech-a11y/0121fcbd45fe900738067a47c3b1a34b/raw/973adeedd0f7b53dbd07f02bc31ac8d589dfdf98/gistfile1.txt"))()
